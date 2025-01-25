49afb3ee3bd931e8ab42b9f88976ef49008540f9
Danil <danillukin161100@gmail.com>
2025-01-25 12:09:43 +0300

    remove inline styles

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,6 +3,7 @@ import AppHeader from "../app-header/app-header.jsx";
 import BurgerConstructor from "../burger-constructor/burger-constructor.jsx";
 import BurgerIngridients from "../burger-ingridients/burger-ingridients.jsx";
 
+import styles from './app.module.css';
 import { INGRIDIENTS_API_URL, categories, constructorIngridients } from "../../utils/data.js";
 
 function App() {
@@ -28,11 +29,7 @@ function App() {
 		<>
 			<AppHeader />
 			<div
-				className="container"
-				style={{
-					display: "grid",
-					gridTemplateColumns: "repeat(2, 1fr)",
-				}}
+				className={`${styles.wrap} container`}
 			>
 				<BurgerIngridients
 					categories={categories}
