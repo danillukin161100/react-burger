848f2c41652dad62af4f2e90355756ba52bd8e41
Danil <danillukin161100@gmail.com>
2025-02-02 13:51:56 +0300

    install packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import { StrictMode } from "react";
+// import { compose, createStore, applyMiddleware } from "redux";
 import { createRoot } from "react-dom/client";
 import App from "./components/app/app.jsx";
 
 import "./assets/index.css";
 
+// const composeEnhancers =
+// 	typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+// 		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+// 		: compose;
+
+// const enhancer = composeEnhancers();
+// const store = createStore(rootReducer, enhancer);
+
 createRoot(document.getElementById("root")).render(
 	// <StrictMode>
-		<App />
+	<App />
 	// </StrictMode>
 );
