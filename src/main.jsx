c5c98ff156877f630a344bc2b84cd73acee83774
Danil <danillukin161100@gmail.com>
2025-02-02 13:56:15 +0300

    prepare for redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import { StrictMode } from "react";
-// import { compose, createStore, applyMiddleware } from "redux";
 import { createRoot } from "react-dom/client";
 import App from "./components/app/app.jsx";
 
 import "./assets/index.css";
 
-// const composeEnhancers =
-// 	typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-// 		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-// 		: compose;
-
-// const enhancer = composeEnhancers();
-// const store = createStore(rootReducer, enhancer);
-
 createRoot(document.getElementById("root")).render(
-	// <StrictMode>
-	<App />
-	// </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
