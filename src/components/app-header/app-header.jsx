442e947bd2336eb715a1b03657780a275cee6218
Danil <danillukin161100@gmail.com>
2025-02-18 23:20:14 +0300

    rework header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,39 +1,32 @@
 import styles from "./app-header.module.css";
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import MenuList from "../menu-list/menu-list";
-import MenuItem from "../menu-item/menu-item";
+import { NavLink, useLocation } from "react-router";
 
 function AppHeader() {
+	const { pathname } = useLocation();
+
+	console.log(pathname);
 	return (
 		<header className={`text text_type_main-default ${styles.header}`}>
 			<div className="container">
 				<div className={styles.inner}>
-					<MenuList>
-						<MenuItem link="/">
-							<BurgerIcon
-								type="primary"
-								className="mr-2"
-							/>
+					<nav className={styles.menu}>
+						<NavLink to="/" className={({ isActive }) => `${isActive && styles.active} ${styles.link} p-5`}>
+							<BurgerIcon type={pathname === "/" ? "primary" : "secondary"} className="mr-2" />
 							Конструктор
-						</MenuItem>
-						<MenuItem link="/orders">
-							<ListIcon
-								type="secondary"
-								className="mr-2"
-							/>
+						</NavLink>
+						<NavLink to="/orders" className={({ isActive }) => `${isActive && styles.active} ${styles.link} p-5`}>
+							<ListIcon type={pathname === "/orders" ? "primary" : "secondary"} className="mr-2" />
 							Лента заказов
-						</MenuItem>
-					</MenuList>
+						</NavLink>
+					</nav>
 					<Logo className={styles.logo} />
-					<MenuList justify="right">
-						<MenuItem link="/profile">
-							<ProfileIcon
-								type="secondary"
-								className="mr-2"
-							/>
+					<nav className={`${styles.menu} ${styles.right}`}>
+						<NavLink to="/profile" className={({ isActive }) => `${isActive && styles.active} ${styles.link} p-5`}>
+							<ProfileIcon type={pathname === "/profile" ? "primary" : "secondary"} className="mr-2" />
 							Личный кабинет
-						</MenuItem>
-					</MenuList>
+						</NavLink>
+					</nav>
 				</div>
 			</div>
 		</header>
